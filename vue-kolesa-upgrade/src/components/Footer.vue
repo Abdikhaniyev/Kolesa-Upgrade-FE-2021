d9abaebe1e505a1123cc7a4b00f120cbb82ed9ad
Alisher <alisher147741@gmail.com>
2021-10-27 23:40:24 +0600

    Создал компонент подвала

---
+++ vue-kolesa-upgrade/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__left">
+        <p class="footer__rights">&copy; Kolesa Group</p>
+        <ul class="footer__socials">
+          <li class="footer__social-item">
+            <a class="footer__social-link" href="#">
+              <img src="@/assets/images/inst.svg" alt="instagram logo" />
+            </a>
+          </li>
+          <li class="footer__social-item">
+            <a class="footer__social-link" href="#">
+              <img src="@/assets/images/youtube.svg" alt="youtube logo" />
+            </a>
+          </li>
+          <li class="footer__social-item">
+            <a class="footer__social-link" href="#">
+              <img src="@/assets/images/vk.svg" alt="vk logo" />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer__right">
+        <p class="footer__text">
+          Есть идеи что улучшить?<br />
+          Не знаешь, с кем решить проблему?
+        </p>
+        <a class="footer__link" href="mailto:alisher147741@gmail.com">Написать</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style></style>
